802b9f7c3436231c89eaadedbb42b2b775f1e5ad
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-12 23:14:30 +0100

    Implement handleChange function in BookForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,18 +1,54 @@
+import { useState } from 'react';
+
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   let idx = 0;
 
+  const [book, setBook] = useState({
+    id: Math.floor(Math.random() * 10) + 1,
+    title: '',
+    category: 'Something',
+  });
+
+  const handleChange = (data, actionType) => {
+    if (actionType === 'changeTitle') {
+      setBook({
+        ...book,
+        title: data,
+      });
+    } else if (actionType === 'changeCategory') {
+      setBook({
+        ...book,
+        category: data,
+      });
+    }
+  };
+
+  const handleSubmit = (formData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="BooksForm">
       <form>
-        <input />
-        <select>
+        <input value={book.title} onChange={(e) => handleChange(e.target.value, 'changeTitle')} />
+        <p>
+          Title:
+          {book.title}
+        </p>
+
+        <p>
+          Category:
+          {book.category}
+        </p>
+
+        <select onChange={(e) => handleChange(e.target.value, 'changeCategory')} name="categories">
           {categories.map((item) => {
             idx += 1;
-            return (<option key={idx}>{item}</option>);
+            return (<option value={item} key={idx}>{item}</option>);
           })}
         </select>
-        <button type="submit">Submit</button>
+        <button type="submit" onSubmit={(e) => handleSubmit(e)}>Submit</button>
       </form>
     </div>
   );
