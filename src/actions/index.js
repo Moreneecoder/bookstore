a262a71823222ff3c8a2039b4a0db7c455026b9d
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-12 17:42:54 +0100

    Implement createAction and removeAction actions

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const createAction = (book) => ({
+    type: 'CREATE_BOOK',
+    book,
+  });
+  
+const removeAction = (book) => ({
+    type: 'REMOVE_BOOK',
+    book,
+});
+
+export {createAction, removeAction};
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -39,7 +39,6 @@ BooksList.propTypes = {
   })),
 };
 
-// export default BooksList;
 const mapStateToProps = (state) => ({ books: state });
 
 const mapDispatchToProps = (dispatch) => ({
