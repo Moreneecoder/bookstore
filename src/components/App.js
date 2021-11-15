631a1cf0a85e64df7c19f79543db26e1df91361a
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-15 10:32:23 +0100

    Structure skeleton for navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 // import '../stylesheets/App.css';
+import NavBar from './NavBar';
 import BooksListContainer from '../containers/BooksList';
 import BooksFormContainer from '../containers/BooksForm';
 
 const App = () => (
   <div className="App">
+    <NavBar />
     <BooksListContainer />
     <BooksFormContainer />
   </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => (
+  <div className="Navbar">
+    <div>
+      <h1>BookVault</h1>
+
+      <p>BOOKS</p>
+      <p>CATEGORIES</p>
+    </div>
+
+    <div className="user-avatar">MA</div>
+  </div>
+);
+
+export default NavBar;
